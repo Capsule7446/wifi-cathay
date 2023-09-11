d77c10dbc9c40ee4a788879498d133e8f4c06814
zPaul <paullai0201@gmail.com>
2023-09-11 17:47:03 +0800

    feat(init):

--- modules/config.go
+++ modules/config.go
@@ -1,14 +1,18 @@
 package modules
 
 import (
+	"fmt"
 	"gopkg.in/yaml.v2"
 	"log"
 )
 
 type Config struct {
-	Wifi string `yaml:"wifi"`
-	Time int    `yaml:"time"`
-	Url  string `yaml:"url"`
+	Wifi     string `yaml:"wifi"`
+	Time     int    `yaml:"time"`
+	BaseUrl  string `yaml:"url"`
+	Account  string `yaml:"account"`
+	Password string `yaml:"password"`
+	Url      string `yaml:"-"`
 }
 
 var ConfigData Config
@@ -17,5 +21,6 @@ func Init(data []byte) {
 	if err := yaml.Unmarshal(data, &ConfigData); err != nil {
 		log.Fatalf("无法解析 YAML 数据: %v", err)
 	}
+	ConfigData.Url = ConfigData.BaseUrl + fmt.Sprintf("?username=%s&password=%s", ConfigData.Account, ConfigData.Password)
 	log.Println(ConfigData)
 }
